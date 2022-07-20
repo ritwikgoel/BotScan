2e829a2bf2efb1a09b4c54201311b17d7932c820
Ritwik Goel <ritwikgoel@yahoo.com>
2022-07-20 09:35:39 +0530

    Fix: UI changes

--- app/lib/main.dart
+++ app/lib/main.dart
@@ -43,12 +43,20 @@ class MyApp extends StatelessWidget {
                     colors: [Colors.green, Colors.blue])),
             child: Column(
               children: [
+                const SizedBox(
+                  height: 25.0,
+                  width: 100,
+                ),
                 const Padding(
                   padding: EdgeInsets.all(28.0),
                   child: Text(
                     "Hola!",
                     style: TextStyle(fontSize: 45),
                   ),
+                ),
+                const SizedBox(
+                  height: 50.0,
+                  width: 100,
                 ),
                 ElevatedButton(
                   onPressed: () {
@@ -58,30 +66,53 @@ class MyApp extends StatelessWidget {
                     shape: RoundedRectangleBorder(
                       borderRadius: new BorderRadius.circular(30.0),
                     ),
+                    primary: Colors.transparent,
+                    minimumSize: const Size(200, 75),
                   ),
-                  child: Text("TTS"),
+                  child: Text(
+                    "Text To Speech",
+                    style: TextStyle(fontSize: 25),
+                  ),
+                ),
+                const SizedBox(
+                  height: 50.0,
+                  width: 100,
                 ),
                 ElevatedButton(
                   onPressed: () {
                     Navigator.pushNamed(context, '/stt');
                   },
-                   style: ElevatedButton.styleFrom(
+                  style: ElevatedButton.styleFrom(
                     shape: RoundedRectangleBorder(
-                      borderRadius:  BorderRadius.circular(30.0),
+                      borderRadius: BorderRadius.circular(30.0),
                     ),
+                    minimumSize: const Size(200, 75),
+                    primary: Colors.transparent,
                   ),
-                  child: const Text("Speech to Text"),
+                  child: const Text(
+                    "Speech to Text",
+                    style: TextStyle(fontSize: 25),
+                  ),
+                ),
+                const SizedBox(
+                  height: 50.0,
+                  width: 100,
                 ),
                 ElevatedButton(
                   onPressed: () {
                     Navigator.pushNamed(context, '/bionic');
                   },
-                   style: ElevatedButton.styleFrom(
-                    shape:  RoundedRectangleBorder(
-                      borderRadius:  BorderRadius.circular(30.0),
+                  style: ElevatedButton.styleFrom(
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(30.0),
                     ),
+                    minimumSize: const Size(200, 75),
+                    primary: Colors.transparent,
                   ),
-                  child: const Text("Bionic text"),
+                  child: const Text(
+                    "Bionic text",
+                    style: TextStyle(fontSize: 30),
+                  ),
                 ),
               ],
             ),
